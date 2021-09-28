3db84dd6a22f472de9e4adafbe72d7669c86ac9b
BjoernTroldahl <btrold20@student.aau.dk>
2021-09-28 12:38:55 +0200

    Updated main class with extra for loop for printing the userList data

--- Client/src/com/company/Main.java
+++ Client/src/com/company/Main.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 public class Main {
 
 
-    int[] userList = {1, 2, 3};
+    //int[] userList = {1, 2, 3};
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         GameClient g = new GameClient();
         User[] userList = new User[3]; //Declares the array with all the user as objects
         //for loop for input of credentials for the specific user object
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < userList.length; i++) {
             userList[i] = new User();
             g.askName();
             userList[i].setUserName(g.name);
             g.askPoints();
             userList[i].setPoints(g.nextInt);
-            System.out.println(userList[i].getUserName()+ ": " + userList[i].getPoints());
+
         }
+        System.out.println("\n");
+
+        for (int j = 0; j < userList.length; j++) {
+        System.out.println(userList[j].getUserName()+ ": " + userList[j].getPoints());
+    }
         //prints out all user object in the array
-        System.out.println("User 1: " + userList[0].getUserName() + ": " + userList[0].getPoints());
-        System.out.println("User 2: " + userList[1].getUserName() + ": " + userList[1].getPoints());
-        System.out.println("User 3: " + userList[2].getUserName() + ": " + userList[2].getPoints());
+        //System.out.println("User 1: " + userList[0].getUserName() + ": " + userList[0].getPoints());
+        //System.out.println("User 2: " + userList[1].getUserName() + ": " + userList[1].getPoints());
+        //System.out.println("User 3: " + userList[2].getUserName() + ": " + userList[2].getPoints());
 
 
 
