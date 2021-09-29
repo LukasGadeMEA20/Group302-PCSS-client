ee47fdf8e970f4aaf4cd92b970a4eef8eccf1503
BjoernTroldahl <btrold20@student.aau.dk>
2021-09-29 14:29:49 +0200

    Made the initial choosePrompt function

--- Client/src/com/company/Main.java
+++ Client/src/com/company/Main.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 
 public class Main {
 
-
-    //int[] userList = {1, 2, 3};
     public static void main(String[] args) {
-        // write your code here
         GameClient g = new GameClient();
         User u = new User();
         int sizeOfArr=3;
-        ArrayList<User>userlist=new ArrayList<User>(sizeOfArr);
-        //User[] userList = new User[3]; //Declares the arraylist with all the users as objects
-        //for loop for input of credentials for the specific user object
-        for (int i = 0; i < 3; i++) {
+        ArrayList<User>userlist=new ArrayList<User>(sizeOfArr); //Declares the arraylist with all the users as objects
+        Prompt p=new Prompt();
+
+        p.choosePrompt();
+
+        for (int i = 0; i < 3; i++) {   //for loop for input of credentials for the specific user object
             userlist.add(new User());
             g.askName();
             userlist.get(i).setUserName(g.name);
@@ -28,10 +27,6 @@ public class Main {
         for (int j = 0; j < 3; j++) {//uses another for loop for printing the data for each User in the userList
         System.out.println(userlist.get(j).getUserName()+ ": " + userlist.get(j).getPoints());
     }
-        //prints out all user object in the array
-        //System.out.println("User 1: " + userList[0].getUserName() + ": " + userList[0].getPoints());
-        //System.out.println("User 2: " + userList[1].getUserName() + ": " + userList[1].getPoints());
-        //System.out.println("User 3: " + userList[2].getUserName() + ": " + userList[2].getPoints());
 
 
 
--- Client/src/com/company/Prompt.java
+++ Client/src/com/company/Prompt.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.Random;
 
 public class Prompt {
     String promptText;
@@ -7,11 +8,17 @@ public class Prompt {
     String prompt2 = "I was afraid of (blank)";
     
     String[] prompts = {prompt1, prompt2};
+
+    Random random=new Random();
+    int min=1;
+    int max=2;
     
     
-    
-    public String choosePrompt(){
-    
+    public void choosePrompt() {
+        for(int i = 1; i <=10; i++) {
+            int randomPrompt = random.nextInt(prompts.length);
+            System.out.println(prompts[randomPrompt]);
+        }
     }
     
     public void delegatePoints(){
