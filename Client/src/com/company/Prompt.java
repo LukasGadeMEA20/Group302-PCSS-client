4a6773c267a67681de40fe2dfe3438385cb8f6ec
Mikkel Sloth Lauridsen <milaur20@student.aau.dk>
2021-09-29 14:00:20 +0200

    Added prompt class

---
+++ Client/src/com/company/Prompt.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Prompt {
+    String promptText;
+    
+    String prompt1 = "Tell a funny joke!";
+    String prompt2 = "I was afraid of (blank)";
+    
+    String[] prompts = {prompt1, prompt2};
+    
+    
+    
+    public String choosePrompt(){
+    
+    }
+    
+    public void delegatePoints(){
+
+    }
+}
