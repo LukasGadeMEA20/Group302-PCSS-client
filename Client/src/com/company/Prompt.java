4b27448afce15d7f9ee898662af2f2b46368dd7c
Mikkel Sloth Lauridsen <milaur20@student.aau.dk>
2021-09-29 14:07:16 +0200

    Update Prompt.java

--- Client/src/com/company/Prompt.java
+++ Client/src/com/company/Prompt.java
@@ -1,20 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Prompt {
     String promptText;
-    
+
     String prompt1 = "Tell a funny joke!";
     String prompt2 = "I was afraid of (blank)";
-    
+
     String[] prompts = {prompt1, prompt2};
-    
-    
-    
-    public String choosePrompt(){
-    
-    }
-    
-    public void delegatePoints(){
+
+    String randomPrompt;
+
+
+    public void choosePrompt(){
 
     }
+
+    public void askPrompt(){
+        System.out.print(randomPrompt);
+        java.util.Scanner in = new Scanner(System.in);
+        name = in.next();
+    }
+    }
+
+
 }
