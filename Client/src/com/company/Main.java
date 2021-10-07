d1234af6db4d3652465b1ea52055f49336bfba35
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2021-10-07 15:37:14 +0200

    GADE SUR

--- Client/src/com/company/Main.java
+++ Client/src/com/company/Main.java
@@ -1,5 +1,44 @@
 package com.company;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scannerInput= new Scanner(System.in);
+        boolean connect = true;
+
+        try {
+            Socket connectToServer = new Socket("localHost", 12345);
+
+            DataInputStream input = new DataInputStream(connectToServer.getInputStream());
+            DataOutputStream output = new DataOutputStream(connectToServer.getOutputStream());
+            while(connect){
+                System.out.println("Enter username");
+                String userName = scannerInput.next();
+
+                output.writeUTF(userName);
+
+                output.flush();
+
+                System.out.print("Do you wish to continue with a new set of values? ");
+                if(scannerInput.next().equals("no")){
+                    connect = false;
+                }
+            }
+            scannerInput.close();
+            connectToServer.close();
+        } catch(IOException e){
+            System.out.println(e.toString());
+        }
+    }
+}
+/*package com.company;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -36,4 +75,4 @@ public class Main {
 
 
     }
-}
+}*/
