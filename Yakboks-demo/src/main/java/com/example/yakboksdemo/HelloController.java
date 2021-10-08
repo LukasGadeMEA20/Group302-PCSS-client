663cf78d74dfcf9b38e31b21abb94f90352f45ba
Mikkel Sloth Lauridsen <milaur20@student.aau.dk>
2021-10-08 14:52:14 +0200

    Reworked application

--- Yakboks-demo/src/main/java/com/example/yakboksdemo/HelloController.java
+++ Yakboks-demo/src/main/java/com/example/yakboksdemo/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.yakboksdemo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    private Label l;
+    private Label b;
+    @FXML private TextField input;
+    @FXML private Label output;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick(ActionEvent event) {
+        output.setText(input.getText());
+        System.out.println(input.getText());
     }
 }
