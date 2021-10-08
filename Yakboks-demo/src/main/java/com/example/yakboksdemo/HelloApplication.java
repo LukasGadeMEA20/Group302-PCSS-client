169282087c47f70c8d14015dbc2818c38a98bd93
Mikkel Sloth Lauridsen <milaur20@student.aau.dk>
2021-10-08 13:46:40 +0200

    Added textfield

--- Yakboks-demo/src/main/java/com/example/yakboksdemo/HelloApplication.java
+++ Yakboks-demo/src/main/java/com/example/yakboksdemo/HelloApplication.java
@@ -1,23 +1,59 @@
 package com.example.yakboksdemo;
 
+// Java program to create a textfield and add a
+// event handler to handle the event of textfield
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+    // launch the application
+    public void start(Stage s)
+    {
+        // set title for the stage
+        s.setTitle("creating textfield");
+
+        // create a textfield
+        TextField b = new TextField("initial text");
+
+        // create a tile pane
+        TilePane r = new TilePane();
+
+        // create a label
+        Label l = new Label("no text");
+
+        // action event
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                l.setText(b.getText());
+            }
+        };
+
+        // when enter is pressed
+        b.setOnAction(event);
+
+        // add textfield
+        r.getChildren().add(b);
+        r.getChildren().add(l);
+
+        // create a scene
+        Scene sc = new Scene(r, 200, 200);
+
+        // set the scene
+        s.setScene(sc);
+
+        s.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String args[])
+    {
+        // launch the application
+        launch(args);
     }
 }
