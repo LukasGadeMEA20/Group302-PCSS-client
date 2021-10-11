51d9bc7dc355a39dad1ffae7ca1657806f4ae1c0
BjoernTroldahl <btrold20@student.aau.dk>
2021-10-11 11:57:06 +0200

    Added parent function in HelloController

--- Yakboks-demo/src/main/java/com/example/yakboksdemo/HelloController.java
+++ Yakboks-demo/src/main/java/com/example/yakboksdemo/HelloController.java
@@ -2,15 +2,22 @@ package com.example.yakboksdemo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.Parent;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML private TextField input;
     @FXML private Label output;
     @FXML
-    protected void onHelloButtonClick(ActionEvent event) {
-        output.setText(input.getText());
+    protected void onHelloButtonClick(ActionEvent event) throws IOException {
+        //output.setText(input.getText());
         System.out.println(input.getText());
+        Parent scene_2_parent= FXMLLoader.load(getClass().getResource("scene2.fxml"));
+        Scene scene2=new Scene(scene_2_parent);
     }
 }
--- Yakboks-demo/src/main/java/com/example/yakboksdemo/YakboksApplication.java
+++ Yakboks-demo/src/main/java/com/example/yakboksdemo/YakboksApplication.java
@@ -23,24 +23,24 @@ public class YakboksApplication extends Application {
         primaryStage.setTitle("My First JavaFX GUI");
 
 //Scene 1
-        /*
+
         Label label1= new Label("This is the first scene");
         Button button1= new Button("Go to scene 2");
         button1.setOnAction(e -> primaryStage.setScene(scene2));
         VBox layout1 = new VBox(20);
         layout1.getChildren().addAll(label1, button1);
         scene1= new Scene(layout1, 300, 250);
-*/
+
             FXMLLoader fxmlLoader = new FXMLLoader(YakboksApplication.class.getResource("scene1.fxml"));
             Scene scene1 = new Scene(fxmlLoader.load(), 320, 240);
-            primaryStage.setTitle("Hello!");
+            primaryStage.setTitle("Page switcher");
             primaryStage.setScene(scene1);
             primaryStage.show();
 
-        Button button1= new Button("Go to scene 2");
-        button1.setOnAction(e -> primaryStage.setScene(scene2));
+        Button button= new Button("Go to scene 2");
+        button.setOnAction(e -> primaryStage.setScene(scene2));
 //Scene 2
-        /*
+
         Label label2= new Label("This is the second scene");
         Button button2= new Button("Go to scene 1");
         button2.setOnAction(e -> primaryStage.setScene(scene1));
@@ -52,7 +52,7 @@ public class YakboksApplication extends Application {
         primaryStage.setScene(scene1);
         primaryStage.show();
 
-         */
+
     }
 
     public static void main(String[] args) {
