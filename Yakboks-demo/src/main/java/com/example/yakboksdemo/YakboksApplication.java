c7263cd4fbeb2c6104b2b8de68736ffc59718728
chris68922 <creime20@student.aau.dk>
2021-10-27 15:10:39 +0200

    ...

--- Yakboks-demo/src/main/java/com/example/yakboksdemo/YakboksApplication.java
+++ Yakboks-demo/src/main/java/com/example/yakboksdemo/YakboksApplication.java
@@ -18,23 +18,8 @@ import java.io.IOException;
 
 public class YakboksApplication extends Application {
 
-    Scene scene1, scene2, scene3, scene4, scene5, scene6, scene7;
-
     @Override
     public void start(Stage primaryStage) throws IOException {
-
-        primaryStage.setTitle("My First JavaFX GUI");
-
-//Scene 1
-
-        Button button1= new Button("Go to scene 2");
-        button1.setOnAction(e -> primaryStage.setScene(scene2));
-        Button button3 = new Button("Submit");
-        TextField textField1 = new TextField();
-        Text text1 = new Text("");
-        AnchorPane layout1 = new AnchorPane();
-        layout1.getChildren().addAll(textField1, button1);
-        scene1= new Scene(layout1, 1920, 1080);
 
             FXMLLoader fxmlLoader = new FXMLLoader(YakboksApplication.class.getResource("scene1.fxml"));
             Scene scene1 = new Scene(fxmlLoader.load(), 1920, 1080);
@@ -42,22 +27,7 @@ public class YakboksApplication extends Application {
             primaryStage.setScene(scene1);
             primaryStage.show();
 
-        Button button= new Button("Go to scene 2");
-        button.setOnAction(e -> primaryStage.setScene(scene2));
-//Scene 2
-
-        Label label2 = new Label("Name: ");
-        Button button2= new Button("Go to scene 1");
-        button2.setOnAction(e -> primaryStage.setScene(scene1));
-        AnchorPane layout2 = new AnchorPane();
-        layout2.getChildren().addAll(label2, button2);
-        scene2= new Scene(layout2,1920,1080);
-
-
-        primaryStage.setScene(scene1);
-        primaryStage.show();
-
-        //Closes the window with esc button
+            //Closes the window with esc button
         primaryStage.addEventHandler(KeyEvent.KEY_RELEASED, (KeyEvent event) -> {
             if (KeyCode.ESCAPE == event.getCode()) {
                 primaryStage.close();
